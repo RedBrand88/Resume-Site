c56d0d98f974ce29efc0301aa6af3b8d3f413d8d
Brandon Bashein <ftl@Brandons-MacBook-Pro.local>
2019-11-15 15:02:29 -0700

    added address to serializers

--- backend/src/resume/api/serializers.py
+++ backend/src/resume/api/serializers.py
@@ -5,4 +5,10 @@ from resume.models import Resume
 class ResumeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Resume
-        fields = ('name', 'phone_number', 'email')
+        fields = (
+            'first_name',
+            'last_name',
+            'phone_number',
+            'email',
+            'address'
+        )
