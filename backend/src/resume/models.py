2fc82d813d037af4d1ab920d52b0112ef9eaac39
Brandon Bashein <ftl@Brandons-MacBook-Pro.local>
2019-11-15 15:08:22 -0700

    add 2 columns to resume db table and removed one column

--- backend/src/resume/models.py
+++ backend/src/resume/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 
 class Resume(models.Model):
-    name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
     email = models.EmailField()
     phone_number = models.CharField(max_length=12)
+    address = models.TextField()
+
 
     def __str__(self):
-        return self.name
+        return self.first_name
