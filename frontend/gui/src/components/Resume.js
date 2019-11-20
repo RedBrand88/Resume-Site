b7701888a4d6c10ded3cec8185e62bee36399008
Brandon Bashein <brandon@thefoodtruckleague.com>
2019-11-19 19:43:20 -0700

    added some styles to resume

--- frontend/gui/src/components/Resume.js
+++ frontend/gui/src/components/Resume.js
@@ -3,18 +3,27 @@ import { Card, Avatar } from 'antd';
 
 const { Meta } = Card;
 
+const column1 = {
+    width: '25%'
+};
+
+const column2 = {
+    width: '75%',
+    textAlign: 'left'
+};
+
 const Resume = (props) => {
     return (
-        <div>
-            <Card title={props.user.first_name + " " + props.user.last_name} extra={<a href="#">More</a>}>
-                <Meta
-                    avatar={<Avatar shape="square" size={64} icon="user" src="https://thefoodtruckleague.com/wp-content/uploads/2015/10/Food-truck-league-logo-01.png" />}
-                />
+        <Card title={props.user.first_name + " " + props.user.last_name} extra={<a href="#">More</a>} >
+            <Card.Grid hoverable={false} style={column1}>
+                <Avatar shape="square" size="large" icon="user" src="https://thefoodtruckleague.com/wp-content/uploads/2015/10/Food-truck-league-logo-01.png" />
+            </Card.Grid>
+            <Card.Grid hoverable={false} style={column2}>
                 <p>{props.user.phone_number}</p>
                 <p>{props.user.email}</p>
                 <p>{props.user.address}</p>
-            </Card>
-        </div>
+            </Card.Grid>
+        </Card>
     );
 }
 
--- frontend/gui/src/containers/BaseLayout.js
+++ frontend/gui/src/containers/BaseLayout.js
@@ -7,26 +7,25 @@ const CustomLayout = (props) => {
 
     return (
         <Layout className="layout">
-            <Header>
+            <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
                 <div className="logo" />
                 <Menu
                     theme="dark"
                     mode="horizontal"
-                    defaultSelectedKeys={['2']}
                     style={{ lineHeight: '64px' }}
                 >
-                    <Menu.Item key="1">nav 1</Menu.Item>
-                    <Menu.Item key="2">nav 2</Menu.Item>
-                    <Menu.Item key="3">nav 3</Menu.Item>
+                    <Menu.Item key="1">Login</Menu.Item>
+                    {/* <Menu.Item key="2">nav 2</Menu.Item> */}
+                    {/* <Menu.Item key="3">nav 3</Menu.Item> */}
                 </Menu>
             </Header>
-            <Content style={{ padding: '0 50px' }}>
+            <Content style={{ padding: '0 50px 50px 50px', background: 'linear-gradient(to right, #f83600 0%, #f9d423 100%)', marginTop: 64 }}>
                 <Breadcrumb style={{ margin: '16px 0' }}>
                     <Breadcrumb.Item>Home</Breadcrumb.Item>
-                    <Breadcrumb.Item>List</Breadcrumb.Item>
-                    <Breadcrumb.Item>App</Breadcrumb.Item>
+                    {/* <Breadcrumb.Item>List</Breadcrumb.Item> */}
+                    <Breadcrumb.Item>Resume</Breadcrumb.Item>
                 </Breadcrumb>
-                <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+                <div style={{ background: '#111', opacity: 1, padding: 24, minHeight: 880 }}>
                     {props.children}
                 </div>
             </Content>
