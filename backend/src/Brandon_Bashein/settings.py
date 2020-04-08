1479d1de98dc1e4a9a0e4f2d7e8eafed2fadc273
RedBrand88 <brandon@thefoodtruckleague.com>
2020-04-08 16:28:19 -0600

    change name of static url

--- backend/src/Brandon_Bashein/settings.py
+++ backend/src/Brandon_Bashein/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
