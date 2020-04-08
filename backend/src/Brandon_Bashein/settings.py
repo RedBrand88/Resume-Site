1f0e65abdbec7c9659fcf22ebb3a155cc66278e0
RedBrand88 <brandon@thefoodtruckleague.com>
2020-04-08 17:01:45 -0600

    add debug logic for environment checking

--- backend/src/Brandon_Bashein/settings.py
+++ backend/src/Brandon_Bashein/settings.py
@@ -19,14 +19,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ohpx)gb#ebhan1&9l3fs*5p$9-q!f%20+xj*#!_cb0ytp+2z4@'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'ohpx)gb#ebhan1&9l3fs*5p$9-q!f%20+xj*#!_cb0ytp+2z4@'
+    ALLOWED_HOSTS = ['*']
 
 # Application definition
 
