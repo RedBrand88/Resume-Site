4f846e5fe6069b3943d3ebccc2c659310e8f3a1a
RedBrand88 <brandon@thefoodtruckleague.com>
2020-04-08 16:30:13 -0600

    add static url

--- backend/src/Brandon_Bashein/settings.py
+++ backend/src/Brandon_Bashein/settings.py
@@ -147,6 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
